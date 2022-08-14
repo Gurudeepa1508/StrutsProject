cdb10170affc5b007ec9657a3ab81a9a3a854742
Jaisakthi <jaisakthi@facilio.com>
2022-08-14 22:04:26 +0530

    Initial commit

---
+++ src/main/webapp/WEB-INF/java/login/LoginAction.java
@@ -0,0 +1,12 @@
+package com;
+//package com.mkyong.common.action;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction  {
+
+    public String execute ()
+
+    {
+        return "success";
+    }
+}
